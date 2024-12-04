f246053c364fd2b1d928d6eb9db12e66ae147f1b
Dnyanesh More <dnyaneshmore77@gmail.com>
2024-12-04 23:03:41 +0530

    Create Week4Assignment1.java

---
+++ Week-4/Week4Assignment1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class Week4Assignment1 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        String courseName = scanner.nextLine();
+
+        out.println("Course: " + courseName);
+    }
+}
