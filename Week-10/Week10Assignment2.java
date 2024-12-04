3e4dd3f92e7076f2d06735e36d3d202b1046f642
Dnyanesh More <dnyaneshmore77@gmail.com>
2024-12-04 23:37:03 +0530

    Add files via upload

---
+++ Week-10/Week10Assignment2.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Week10Assignment2 
+{
+    public static void main(String args[]) 
+    {
+        try 
+        {
+            Connection conn = null;
+            String DB_URL = "jdbc:sqlite:/tempfs/db";
+            System.setProperty("org.sqlite.tmpdir", "/tempfs");
+          
+            conn = DriverManager.getConnection(DB_URL);
+
+            if (conn.isClosed())
+                System.out.print("false");
+            else
+                System.out.print("true");
+
+            Scanner sc = new Scanner(System.in);
+            int s = sc.nextInt();
+            if (s == 7) 
+            {
+                conn.close(); 
+                System.out.print(conn.isValid(1));
+            }
+        } 
+        catch (Exception e) 
+        {
+            System.out.println(e);
+        } 
+    }
+}
