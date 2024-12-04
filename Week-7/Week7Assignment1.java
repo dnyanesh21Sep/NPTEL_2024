4708710cbf87793dc616b0082af71552632f91e7
Dnyanesh More <dnyaneshmore77@gmail.com>
2024-12-04 23:28:26 +0530

    Create Week7Assignment1.java

---
+++ Week-7/Week7Assignment1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Week7Assignment1 
+{
+  public static void main(String[] args) 
+  {
+    int num1, num2, num3, sum = 0;
+    
+    Scanner in = new Scanner(System.in);
+    num1 = in.nextInt();
+    num2 = in.nextInt();
+    num3 = in.nextInt();
+    sum = num1 + num2 + num3;
+    System.out.println(sum);
+  }
+}
